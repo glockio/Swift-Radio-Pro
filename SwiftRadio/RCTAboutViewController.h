26778704f94049468017dd4b6e8fd40577c5310c
Greg Laughlin <greg.laughlin81@hotmail.com>
2016-03-20 12:56:56 -0700

    Add readme notes after lecture

---
+++ SwiftRadio/RCTAboutViewController.h
@@ -0,0 +1,13 @@
+//
+//  RCTAboutViewController.h
+//  SwiftRadio
+//
+//  Created by Gregory Laughlin on 2016-03-20.
+//  Copyright © 2016 CodeMarket.io. All rights reserved.
+//
+
+#ifndef RCTAboutViewController_h
+#define RCTAboutViewController_h
+
+
+#endif /* RCTAboutViewController_h */
---
+++ SwiftRadio/RCTAboutViewController.m
@@ -0,0 +1,9 @@
+//
+//  RCTAboutViewController.m
+//  SwiftRadio
+//
+//  Created by Gregory Laughlin on 2016-03-20.
+//  Copyright © 2016 CodeMarket.io. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
