7b182c5eaddc0509e80a29ea8e6af6eee6ca42cd
greg-laughlin-hs <greg.laughlin@hootsuite.com>
2016-04-05 14:48:12 -0700

    Update play controls active opacity state

--- ReactComponents/playControls.js
+++ ReactComponents/playControls.js
@@ -29,16 +29,16 @@ class PlayControls extends React.Component {
   componentDidMount() {
     const {props} = this
 
-    // Register native Event Listner
-    this.onLoadStateChangeSubscription = DeviceEventEmitter.once("Playable.onLoadStateChange", (nativeEvent) => {
-      this.setState({isLoading: false})
-    })
-    // Setting station blocks main thread
-    // Requesting frame ensures we flush the q once before we run
-    // MediaPlayer.setStation
-    this.frameRequest = requestAnimationFrame( () => {
-      MediaPlayer.setStation(props)
-    })
+    // // Register native Event Listner
+    // this.onLoadStateChangeSubscription = DeviceEventEmitter.once("Playable.onLoadStateChange", (nativeEvent) => {
+    //   this.setState({isLoading: false})
+    // })
+    // // Setting station blocks main thread
+    // // Requesting frame ensures we flush the q once before we run
+    // // MediaPlayer.setStation
+    // this.frameRequest = requestAnimationFrame( () => {
+    //   MediaPlayer.setStation(props)
+    // })
   }
 
   componentWillUnmount() {
@@ -62,22 +62,29 @@ class PlayControls extends React.Component {
     })
   }
 
-  render(){
-    let pauseButtonOpacity, playButtonOpacity = 1
-    if(this.state.isPlaying) {
-      pauseButtonOpacity = 0.5
-    } else {
-      playButtonOpacity = 0.5
-    }
+  renderPauseButton(opacity) {
+   return (
+      <TouchableHighlight onPress= { () => this._pause()} activeOpacity={0.80} underlayColor={'transparent'} style={{opacity: opacity}}>
+        <Image source={{uri: 'btn-pause'}} style={{width: 45, height: 45, marginRight: 10}}/>
+      </TouchableHighlight>
+    )
+  }
 
+  renderPlayButton(opacity) {
+    return(
+      <TouchableHighlight onPress={ () => this._play()} activeOpacity={0.80} underlayColor={'transparent'} style={{opacity: opacity}}>
+        <Image source={{uri: 'btn-play'}} style={{width: 45, height: 45, marginRight: 10}}/>
+      </TouchableHighlight>
+    )
+  }
+
+  render(){
+    const pauseButton = this.state.isPlaying ? this.renderPauseButton(1.0) : this.renderPauseButton(0.5)
+    const playButton  = this.state.isPlaying ? this.renderPlayButton(0.5) : this.renderPlayButton(1.0)
     return(
       <View style={{flex:1, alignItems:'center', justifyContent:'center', flexDirection:'row',overflow:'hidden'}}>
-        <TouchableHighlight onPress= { () => this._pause()} activeOpacity={0.80} underlayColor={'transparent'}>
-          <Image source={{uri: 'btn-pause'}} style={{width: 45, height: 45, marginRight: 10, opacity:pauseButtonOpacity}}/>
-        </TouchableHighlight>
-        <TouchableHighlight onPress={ () => this._play()} activeOpacity={0.80} underlayColor={'transparent'}>
-          <Image source={{uri: 'btn-play'}} style={{width: 45, height: 45, opacity:playButtonOpacity}}/>
-        </TouchableHighlight>
+          {pauseButton}
+          {playButton}
       </View>
     )
   }
